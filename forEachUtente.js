5ad1e44b432e2f855f90c7946ee160db335e0b66
ang <angelo03giuliano@gmail.com>
2024-01-19 20:01:54 +0100

    up

--- forEachUtente.js
+++ forEachUtente.js
@@ -1,10 +1,10 @@
 export const forEachUtente = (utente) => {
   main.innerHTML += `
-        <div class="ms-3 card" style="width: 18rem;">
+        <div class="m-3 card" style="width: 18rem;">
         <div class="card-body">
-          <p class="card-title"><b>Name:</b> ${utente.name}</p>
           <p class="card-text"><b>Username:</b> ${utente.username}</p>
           <p class="card-text"><b>Email:</b> ${utente.email.toLowerCase()}</p>
+          <p class="card-title"><b>Name:</b> ${utente.name}</p>
           <p class="card-text"><b>Address:</b> ${utente.address.city}</p>
           <p class="card-text"><b>Phone:</b> ${utente.phone}</p>
         </div>
